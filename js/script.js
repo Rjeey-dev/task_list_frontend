95e70ef2d8e23c8103b254b9c3ac1a1609301895
Rjeey-dev <valera.abashyn@gmail.com>
2021-03-24 13:03:51 +0300

    finished frontend

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-(function(angular) {
+(function (angular) {
     'use strict';
     const app = angular.module("todo", []);
 
@@ -8,33 +8,31 @@
     const TASK_STATUS_DOING = 1;
     const TASK_STATUS_DONE = 2;
 
-    app.controller("mainController", function($scope, $http) {
+    app.controller("mainController", function ($scope, $http) {
         $scope.comments = [];
         $scope.tasks = [];
         $scope.todoTasks = [];
         $scope.doingTasks = [];
         $scope.doneTasks = [];
 
-        $scope.loadTasks = function() {
+        $scope.loadTasks = function () {
             $scope.tasks = [];
 
-            $http.get(apiUri + '/tasks').
-            then(function success(response) {
+            $http.get(apiUri + '/tasks').then(function success(response) {
                     $scope.tasks = response.data.items;
                     $scope.syncTasks();
-                }, function error(response){
+                }, function error(response) {
                     console.log("Error");
                     console.log("response code: " + response.status);
                 }
             );
         };
 
-        $scope.startEdit = function(task) {
+        $scope.startEdit = function (task) {
             $scope.comments = [];
-            $http.get(apiUri + '/comments?task_id=' + task.id).
-            then(function success(response) {
+            $http.get(apiUri + '/comments?task_id=' + task.id).then(function success(response) {
                     $scope.comments = response.data.items;
-                }, function error(response){
+                }, function error(response) {
                     console.log("Error");
                     console.log("response code: " + response.status);
                 }
@@ -43,70 +41,71 @@
             $scope.chooseCurrentTask(task);
         };
 
-        $scope.addComment = function() {
-            $http.post(apiUri + '/comments', {text: $scope.createCommentText, task_id: $scope.currentTask.id}).
-            then(function success(response) {
+        $scope.addComment = function () {
+            $http.post(apiUri + '/comments', {
+                text: $scope.createCommentText,
+                task_id: $scope.currentTask.id
+            }).then(function success(response) {
                     $scope.comments.push(response.data.details);
-                }, function error(response){
+                }, function error(response) {
                     console.log("Error");
                     console.log("response code: " + response.status);
                 }
             );
         };
 
-        $scope.chooseCurrentTask = function(task) {
+        $scope.chooseCurrentTask = function (task) {
             $scope.currentTask = task;
         };
 
-        $scope.delete = function() {
-            $http.delete(apiUri + '/tasks/' + $scope.currentTask.id).
-            then(function success(response) {
+        $scope.delete = function () {
+            $http.delete(apiUri + '/tasks/' + $scope.currentTask.id).then(function success(response) {
                     $scope.tasks.forEach((task, key) => {
                         if ($scope.currentTask.id === task.id) {
                             $scope.tasks.splice(key, 1);
                             $scope.syncTasks();
                         }
                     });
-                }, function error(response){
+                }, function error(response) {
                     console.log("Error");
                     console.log("response code: " + response.status);
                 }
             );
         };
 
-        $scope.changeStatus = function(currentTask, status) {
-            $http.patch(apiUri + '/tasks/' + currentTask.id, {status: status}).
-            then(function success(response) {
-                $scope.tasks.forEach((task, key) => {
-                    if (currentTask.id === task.id) {
-                        $scope.tasks.splice(key, 1);
-                        $scope.tasks.push(response.data.details);
-                        $scope.syncTasks();
-                    }
-                });
-                }, function error(response){
+        $scope.changeStatus = function (currentTask, status) {
+            $http.patch(apiUri + '/tasks/' + currentTask.id, {status: status}).then(function success(response) {
+                    $scope.tasks.forEach((task, key) => {
+                        if (currentTask.id === task.id) {
+                            $scope.tasks.splice(key, 1);
+                            $scope.tasks.push(response.data.details);
+                            $scope.syncTasks();
+                        }
+                    });
+                }, function error(response) {
                     console.log("Error");
                     console.log("response code: " + response.status);
                 }
             );
         };
 
-        $scope.update = function(i) {
-            $http.patch(apiUri + '/tasks/' + $scope.currentTask.id, {name: $scope.currentTask.name, status: $scope.currentTask.status}).
-            then(function success(response) {
-                }, function error(response){
+        $scope.update = function (i) {
+            $http.patch(apiUri + '/tasks/' + $scope.currentTask.id, {
+                name: $scope.currentTask.name,
+                status: $scope.currentTask.status
+            }).then(function success(response) {
+                }, function error(response) {
                     console.log("Error");
                     console.log("response code: " + response.status);
                 }
             );
         };
 
-        $scope.addTask = function() {
-            $http.post(apiUri + '/tasks', {'name': $scope.createTodoTaskName}).
-            then(function success(response) {
+        $scope.addTask = function () {
+            $http.post(apiUri + '/tasks', {'name': $scope.createTodoTaskName}).then(function success(response) {
                     $scope.tasks.push(response.data.details);
                     $scope.syncTasks();
-                }, function error(response){
+                }, function error(response) {
                     console.log("Error");
                     console.log("response code: " + response.status);
                 }
@@ -118,7 +117,7 @@
             $scope.doingTasks = [];
             $scope.doneTasks = [];
 
-            angular.forEach($scope.tasks, function(task) {
+            angular.forEach($scope.tasks, function (task) {
                 switch (task.status) {
                     case TASK_STATUS_TODO:
                         $scope.todoTasks.push(task);
